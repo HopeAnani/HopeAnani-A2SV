7d87ba4ddd2cc8f11055c761fc38ec87565dd9b6
Anani Samuel <94631710+HopeAnani@users.noreply.github.com>
2022-09-24 12:36:54 +0300

    Leetcode: Reverse Substrings Between Each Pair of Parentheses.

---
+++ Reverse Substrings Between Each Pair of Parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        int n = s.length();
+        vector<int> opened, pair(n);
+        
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == '(')
+                opened.push_back(i);
+            if (s[i] == ')') {
+                int j = opened.back();
+                opened.pop_back();
+                pair[i] = j;
+                pair[j] = i;
+            }
+        }
+        string result;
+        for (int i = 0, d = 1; i < n; i += d) {
+            if (s[i] == '(' || s[i] == ')')
+                i = pair[i], d = -d;
+            else
+                result += s[i];
+        }
+        return result;
+    }
+};
